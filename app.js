c0cbcef78321cc4f1d8094b45c849dbc2e9f5db1
Антон Истомин <istomanton@gmail.com>
2015-07-05 00:30:13 +0500

    first dirty working version

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
 
-// app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 8080));
-app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-    // res.render('home', {
-    //   title: 'Welcome'
-    // });
-    res.render('index.html');
+    res.render('home');
+});
+
+app.get('/donate', function(req, res) {
+    res.render('donate.html');
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact');
 });
 
 app.listen(app.get('port'), function() {
