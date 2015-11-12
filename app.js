60a8b8548c9eb61a2bb1a208524833e0a745f5b3
aionics <aionics@list.ru>
2015-11-12 16:49:40 +0300

    Revert master to working version without mysql

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
 var express = require('express');
 var fs = require('fs');
-var logger = require('./libs/logger');
-var content = require('./domain/content');
-
+var logger = require('./libs/logger.js');
 var app = express();
-var LOG_LEVELS = logger.LOG_LEVELS;
 
+var newsPath = 'public/json/news.json';
+var news = JSON.parse(fs.readFileSync(newsPath));
+var rules = JSON.parse(fs.readFileSync('public/json/rules.json'));
 var plugins = JSON.parse(fs.readFileSync('public/json/plugins.json'));
 
-app.set('port', process.env.PORT || 8080);
+// TODO: Move to library
+fs.watch(newsPath, 
+    function (event, filename) {
+        logger.log('\<fs.watch\> Event \'' + event + '\' happened to file \'' + filename + '\'');
+        
+        if (event == "change") {
+            fs.readFile(newsPath, 'utf-8', 
+                function(err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    
+                    try {
+                        news = JSON.parse(data);
+                        logger.log('File \'' + newsPath + '\' was reloaded.');
+                    }
+                    catch (ex) {
+                        logger.log('Error parsing file \'' + newsPath + '\'');
+                        logger.log('JSON.parse threw exception: ' + ex);
+                    }
+                }
+            );
+        }
+    }
+);
+
+app.set('port', (process.env.PORT || 8080));
 app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
 if (app.get('env') === 'development') {
-    logger.log("HTML pretty format enabled", LOG_LEVELS.DEBUG);
     app.locals.pretty = true;
 }
 
 app.get('/', function(req, res) {
-    res.render('home', {news: content.getNews()});
+    res.render('home', {news: news});
 });
 
 app.get('/donate', function(req, res) {
-    res.render('donate', {donate: content.getDonate()});
+    res.render('donate');
 });
 
 app.get('/contact', function(req, res) {
@@ -31,7 +56,7 @@ app.get('/contact', function(req, res) {
 });
 
 app.get('/rules', function(req, res) {
-    res.render('rules', {rules: content.getRules()});
+    res.render('rules', {rules: rules});
 });
 
 app.get('/plugins', function(req, res) {
@@ -40,6 +65,4 @@ app.get('/plugins', function(req, res) {
 
 app.listen(app.get('port'), function() {
     logger.log("Node app is running at localhost:" + app.get('port'));
-    logger.log("Environment is '" + app.get('env') + "'");
-    logger.log("Logging level is '" + logger.CURRENT_LOG_LEVEL_NAME + "'");
 });
--- libs/date-util.js
+++ libs/date-util.js
@@ -1,27 +1,7 @@
-function padWithZero(number) {
-    return number < 10 ? '0' + number : number;
-}
-
-// Formats date as YYYY-MM-DD HH:MM:SS
 var format = function(date) {
-    var year = date.getFullYear();
-    var month = padWithZero(date.getMonth() + 1);
-    var day = padWithZero(date.getDate());
-
-    var hour = padWithZero(date.getHours());
-    var minutes = padWithZero(date.getMinutes());
-    var seconds = padWithZero(date.getSeconds());
-
-    return year + '-' + month + '-' + day + ' ' + hour + ':' + minutes + ':' + seconds;
-};
-
-// Formats date as DD.MM.YYYY
-var formatRussianDate = function(date) {
-    var day = padWithZero(date.getDate());
-    var month = padWithZero(date.getMonth() + 1);
-    var year = date.getFullYear();
-
-    return day + '.' + month + '.' + year;
+    // TODO: Format date correctly
+    //return date.toISOString();
+    return date;
 };
 
 var newDate = function() {
@@ -29,5 +9,4 @@ var newDate = function() {
 };
 
 exports.format = format;
-exports.formatRussianDate = formatRussianDate;
 exports.newDate = newDate;
